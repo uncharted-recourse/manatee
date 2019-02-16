ed5d83b9991caa1d47eb69859a836f3cdf879e03
jlgleason <jeffrey.gleason.3@gmail.com>
2019-02-15 19:24:11 -0600

    more testing code, fixed some bugs in methods

--- manatee/robust_rcf.py
+++ manatee/robust_rcf.py
@@ -13,6 +13,7 @@ import pandas as pd
 import rrcf as rrcf_base
 from evaluate import evaluate
 import matplotlib.pyplot as plt
+from tslearn.preprocessing import TimeSeriesScalerMinMax, TimeSeriesScalerMeanVariance
 
 class robust_rcf():
     '''
@@ -60,13 +61,17 @@ class robust_rcf():
         # create forest
         forest = []
 
+        # scale points between 0 and 1 ??? might improve
+        #points = TimeSeriesScalerMinMax().fit_transform(points)
+        #points = TimeSeriesScalerMeanVariance().fit_transform(points)
+
         # take unqiue values before random sampling, so random sample isn't replication of same points
         while len(forest) < self.num_trees:
             # Select random subsets of points uniformly from point set
-                ixs = np.random.choice(self.num_points, size = (self.num_points // self.tree_size, self.tree_size), replace=False)
+                ixs = np.random.choice(self.num_points, self.tree_size, replace=False)
             # Add sampled trees to forest
-                trees = [rrcf_base.RCTree(points[ix], index_labels=ix) for ix in ixs]
-                forest.extend(trees)
+                tree = rrcf_base.RCTree(points[ixs], index_labels=ixs)
+                forest.append(tree)
         self.forest = forest
 
     def batch_anomaly_scores(self):
@@ -95,18 +100,21 @@ class robust_rcf():
         avg_codisp /= index
         return avg_codisp
     
-    def anomaly_score(self, point):
+
+    def anomaly_score(self, points):
         '''
         Computes anomaly score for point by inserting point in each tree in the rcrf and
         computing the average collusive displacement. Higher scores indicate a higher displacement 
-        and thus a higher likelihood of anomaly. It then deletes the point from each tree to 
-        maintain the state of the forest. 
+        and thus a higher likelihood of anomaly. It then deletes points from each tree to 
+        maintain the state of the forest. (If you want new points to be added to trees, you can 
+        use the function stream_anomaly_scores with `new_forest` == False)
 
         Parameters:
-            point:   the point on which to calculate an anomaly score
+            points:   the points on which to calculate anomaly scores. np.ndarray of size (n x d)
 
         Returns:
-            anomaly_score: average collusive displacement (anomaly score) for point
+            anomaly_score: pandas Series with index of points and average collusive 
+                           displacement (anomaly score) for each point
         '''
          # assert that fit function has already been called and a forest exists
         try: 
@@ -114,12 +122,30 @@ class robust_rcf():
         except:
             raise ValueError("Cannot compute anomaly scores on forest that has not been fit")
 
-        avg_codisp = 0
-        for tree in self.forest:
-            tree.insert_point(point, index='point')
-            codisp = tree.codisp('point')
-            avg_codisp += codisp
-            tree.forget_point('point')
+        # assert that points.shape has two dimensions
+        try:
+            assert(len(points.shape) is 2)
+        except:
+            raise ValueError("Points must have shape (n x d)")
+
+        # assert that dimension of points is the same as dimension in fit tree
+        try:
+            assert(points.shape[1] == self.dimension)
+        except:
+            raise ValueError("Forest and points must have the same dimension. Points dim: {} vs. Forest dim: {}".format(points.shape[1], self.dimension))
+
+        # scale point to between 0 and 1 ??? might improve
+        #points = TimeSeriesScalerMinMax().fit_transform(points)
+        #point = TimeSeriesScalerMeanVariance().fit_transform(pointt)
+
+        avg_codisp = pd.Series(0.0, index=np.arange(points.shape[0]))
+        points = rrcf_base.shingle(points, size = 1)
+        for index, point in enumerate(points):
+            for tree in self.forest:
+                tree.insert_point(point, index='point')
+                codisp = tree.codisp('point')
+                avg_codisp[index] += codisp
+                tree.forget_point('point')
         return avg_codisp // self.num_trees
 
     def stream_anomaly_scores(self, points, window_size, new_forest = False):
@@ -150,6 +176,7 @@ class robust_rcf():
             for _ in range(self.num_trees):
                 tree = rrcf_base.RCTree()
                 forest.append(tree)
+            self.forest = forest
         
         # create rolling window of size window_size
         points_gen = rrcf_base.shingle(points, size=window_size)
@@ -159,9 +186,9 @@ class robust_rcf():
         initial_index = self.num_points
         for index, point in enumerate(points_gen):
             index += initial_index
-            for tree in forest:
+            for tree in self.forest:
                 # If tree is above permitted size, drop the oldest point (FIFO)
-                if len(tree.leaves) > self.tree_size:
+                if len(tree.leaves) >= self.tree_size:
                     tree.forget_point(index - self.tree_size)
                     self.num_points -= 1
                 # Insert the new point into the tree
@@ -170,14 +197,14 @@ class robust_rcf():
                 except:
                     ValueError('failure for point {} at index {}'.format(point, index))
                 self.num_points += 1
-                # Compute codisp and take the average among all trees
+                # Compute codisp on the new point and take the average among all trees
                 avg_codisp[index] += tree.codisp(index)
-        self.forest = forest
         return avg_codisp // self.num_trees
 
 # main method for testing class
 if __name__ == '__main__':
 
+    ''' Preprocessing '''
     # load Twitter hashtag data
     npzfile = np.load("/Users/jeffreygleason 1/Desktop/NewKnowledge/Code/time-series/tapir/tapir/data/prep/hashtags_2019_01_01_to_2019_02_01.npz")
     hashtags = npzfile['rate_vals']
@@ -185,51 +212,67 @@ if __name__ == '__main__':
 
     # mark top 5% of rate values as anomalous
     anom_thresh = 95
-    anom = hashtags[ht] > np.percentile(hashtags[ht], anom_thresh)
+    #anom = hashtags[ht] > np.percentile(hashtags[ht], anom_thresh)
 
+    ''' Instantiate RRCF'''
     # set HPs and instantiate
-    num_trees = 100
-    tree_size = 20
+    num_trees = 200
+    tree_size = 40
     clf = robust_rcf(num_trees, tree_size)
-
+    
+    ''' Test Different Methods'''
     # test batch anomaly detection
     clf.fit_batch(hashtags[ht].reshape(-1,1))
     anom_score = clf.batch_anomaly_scores()
+
+    # test point anomaly detection 
+    # slightly different anomaly scores than batch because inserting every point into every tree 
+    # (vs random sampling in batch)
+    point_scores = clf.anomaly_score(hashtags[ht].reshape(-1,1))
+    
     # mark top 5% of predictions as anomalous
     anom_pred = anom_score > np.percentile(anom_score, anom_thresh)
 
     # print evaluation
-    print(evaluate(anom, anom_pred)) 
+    # print(evaluate(anom, anom_pred)) 
 
     # plot comparison of labeled anomalies to predicted anomalies
     colors = ('blue', 'red')
     targets = ('non-anomalous', 'anomalous')
-    indices = (np.where(~anom), np.where(anom))
-    data = (hashtags[ht][~anom], hashtags[ht][anom])
+    #indices = (np.where(~anom), np.where(anom))
+    #data = (hashtags[ht][~anom], hashtags[ht][anom])
     pred_indices = (np.where(~anom_pred), np.where(anom_pred))
     pred_data = (hashtags[ht][~anom_pred], hashtags[ht][anom_pred])
+    #plt.subplot(2,1,1)
+    #for index, dat, color, target in zip(indices, data, colors, targets):
+    #    plt.scatter(index, dat, c = color, label = target, s=10)
+    #plt.legend()
     plt.subplot(2,1,1)
-    for index, dat, color, target in zip(indices, data, colors, targets):
+    for index, dat, color, target in zip(pred_indices, pred_data, colors, targets):
         plt.scatter(index, dat, c = color, label = target, s=10)
     plt.legend()
+    #plt.show()
+    
+    # test streaming anomaly detection
+    window_size = 1
+    anom_score = clf.stream_anomaly_scores(hashtags[ht], window_size, new_forest = True)
+    # mark top 5% of predictions as anomalous
+    anom_pred = anom_score > np.percentile(anom_score, anom_thresh)
+
+    # print evaluation
+    #print(evaluate(anom, anom_pred)) 
+
+    # plot comparison of labeled anomalies to predicted anomalies
+    pred_indices = (np.where(~anom_pred), np.where(anom_pred))
+    pred_data = (hashtags[ht][~anom_pred], hashtags[ht][anom_pred])
     plt.subplot(2,1,2)
     for index, dat, color, target in zip(pred_indices, pred_data, colors, targets):
         plt.scatter(index, dat, c = color, label = target, s=10)
     plt.legend()
     plt.show()
 
-    
-    # test streaming anomaly detection
-    window_size = 1
-    anom_score = clf.stream_anomaly_scores(hashtags[ht], window_size, True)
-    # mark top 5% of predictions as anomalous
-    anom_pred = anom_score > np.percentile(anom_score, anom_thresh)
-
-    # print evaluation
-    print(evaluate(anom, anom_pred)) 
-
-
-
+    ## TODO: Test streaming anomaly detection from created tree
+    #print(clf.stream_anomaly_scores(hashtags[ht], window_size))
 
 
 
