ba1c6705facd24c95affd0ffa4905056ebd99f4a
Ubuntu <jgleason@JeffreyVMDev.umi1swgzx55e3jlvq0v5igys1h.jx.internal.cloudapp.net>
2019-02-24 22:16:50 +0000

    add latest commit of rrcf in dependency links

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='manatee',
         "Sloth==2.0.5",
         "nltk",
         'Keras >= 2.0.2, <= 2.1.6'],
-    dependency_links=['git+https://github.com/NewKnowledge/rrcf@8164d6fa9787b13b575eb1eaad8d57a775adda90#egg=rrcf-0.1',
+    dependency_links=['git+https://github.com/NewKnowledge/rrcf@1844465f28816b55ef4ef481809dcf26f968c5c3#egg=rrcf-0.1',
         'git+https://github.com/NewKnowledge/simon@e521e0d93c25b275488a98f57acf74c3144afaeb#egg=Simon-1.2.3',
         'git+https://github.com/NewKnowledge/sloth@059f53323f17f64c3fd61ce27e81a074afc492dc#egg=Sloth-2.0.5'],
     include_package_data=True)
