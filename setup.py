309618a6355c8c2267596e50c1b263a6ac643cd8
jlgleason <jeffrey.gleason.3@gmail.com>
2019-02-26 17:26:37 -0600

    add latest version of Sloth / Shapelets

--- setup.py
+++ setup.py
@@ -20,11 +20,11 @@ setup(name='manatee',
         'tslearn>=0.1.21',
         'rrcf==0.1',
         'Simon==1.2.3',
-        "Sloth==2.0.5",
+        "Sloth==2.0.6",
         "nltk",
         'Keras >= 2.0.2, <= 2.1.6'],
     dependency_links=['git+https://github.com/NewKnowledge/tslearn@612b91cc0150d0b2c548f7426a7a4fafb864340e#egg=tslearn'
         'git+https://github.com/NewKnowledge/rrcf@1844465f28816b55ef4ef481809dcf26f968c5c3#egg=rrcf-0.1',
         'git+https://github.com/NewKnowledge/simon@e521e0d93c25b275488a98f57acf74c3144afaeb#egg=Simon-1.2.3',
-        'git+https://github.com/NewKnowledge/sloth@059f53323f17f64c3fd61ce27e81a074afc492dc#egg=Sloth-2.0.5'],
+        'git+https://github.com/NewKnowledge/sloth@281d54176aa3ab4a14bab43a52d141b741074d1c#egg=Sloth-2.0.6'],
     include_package_data=True)
