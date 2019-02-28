58196f68f87b922148e3b9574e22599bf8082a53
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-28 10:58:53 -0600

    create training module

---
+++ manatee/train.py
@@ -0,0 +1,34 @@
+from Sloth.classify import Shapelets
+
+def data_augmentation():
+    '''
+        augment samples of less frequent classes
+        so that all classes have same number of samples
+    '''
+
+def data_augmentation_with_noise():
+    '''
+        augment samples of less frequent classes (with noise)
+        so that all classes have same number of samples
+    '''
+
+def shapelet_sizes_grid_search():
+    '''
+        grid search over different shapelet dictionary options
+        from Grabocka paper
+    '''
+
+def train_shapelets():
+    # data augmentation
+
+    # hyperparameter optimization
+
+    # shapelet sizes grid search
+
+    # epoch optimization with best HPs and shapelet sizes
+    pass
+
+
+# main method for training methods
+if __name__ == '__main__':
+    pass
